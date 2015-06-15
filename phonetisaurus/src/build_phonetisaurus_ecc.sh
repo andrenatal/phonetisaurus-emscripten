7f986d0c9e6cdc73bdc52a04024ef8853a28877c
Andre Natal <anatal@gmail.com>
2015-06-15 01:44:26 -0300

    Working fast and optimized with -O3

--- phonetisaurus/src/build_phonetisaurus_ecc.sh
+++ phonetisaurus/src/build_phonetisaurus_ecc.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 
 rm *.js
-emcc Phonetisaurus.cpp FstPathFinder.cpp MBRDecoder.cpp LatticePruner.cpp util.cpp phonetisaurus-moz.cpp -o phonetisaurus-moz.html openfst.o -I3rdparty/sparsehash -I3rdparty/utfcpp  -I. -I../../openfst-1.4.1/src/include -lfst -ldl -lm -lrt -v -lstdc++  -std=c++11 -s TOTAL_MEMORY=67108864 --preload-file asset_dir   
+emcc -O3 Phonetisaurus.cpp FstPathFinder.cpp MBRDecoder.cpp LatticePruner.cpp util.cpp phonetisaurus-moz.cpp -o phonetisaurus-moz.html openfst.o -I3rdparty/sparsehash -I3rdparty/utfcpp  -I. -I../../openfst-1.4.1/src/include -lfst -ldl -lm -lrt -v -lstdc++  -std=c++11 -s TOTAL_MEMORY=67108864 --preload-file asset_dir   
